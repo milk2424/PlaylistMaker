8b5ca53fcffc6f1afd415e498e21aca33b2857fe
Dmitry <milko.dimon@mail.ru>
2025-09-30 17:00:00 +0300

    hide menu in player

--- app/src/main/java/com/example/playlistmaker/ui/root/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/ui/root/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.playlistmaker.ui.root
 
 import android.os.Bundle
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.ActivityMainBinding
+import com.example.playlistmaker.ui.player.PlayerFragment
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
@@ -13,8 +16,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainerView) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.mainContainerView) as NavHostFragment
         val navController = navHostFragment.navController
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.playerFragment -> binding.bottomNavMenu.visibility = GONE
+                else -> binding.bottomNavMenu.visibility = VISIBLE
+            }
+        }
+
         binding.bottomNavMenu.setupWithNavController(navController)
     }
 }
